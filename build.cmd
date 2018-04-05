29bca07adde3f19522211ea6b9190e4e6c88685b
rflechner <rflechner@romcyber.com>
2018-04-05 21:13:49 +0200

    remove fake

--- build.cmd
+++ build.cmd
@@ -1,8 +1,3 @@
-@rem @echo off
-@rem cls
-@rem ".nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
-@rem "packages\FAKE\tools\Fake.exe" build.fsx %*
-
 dotnet restore
 dotnet test ScrapySharp.Tests\ScrapySharp.Tests.csproj
 dotnet build --configuration release
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-nuget Install FAKE -OutputDirectory packages -ExcludeVersion
-mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
