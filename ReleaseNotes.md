7d15bd8fddc2e177ba7e2634e0cc625c35fe7d59
rflechner <rflechner@romcyber.com>
2018-04-05 22:08:45 +0200

    generate nuget version and release note from build script

--- ReleaseNotes.md
+++ ReleaseNotes.md
@@ -1,2 +1,8 @@
+# 3.0.0-alpha1
+
+- dotnet standard 2.0
+
+# 2.6.2
+
 - Introduce async methods for .Net >= 4.5
 - Add possibility to add special headers for using "NavigateToPage" (Pull Request #6)
--- build.ps1
+++ build.ps1
@@ -1,8 +1,57 @@
+# functions coming from https://blogs.msdn.microsoft.com/jaredpar/2009/01/16/powershell-linq-take-count-and-take-while/
+#============================================================================
+# Take count elements fro the pipeline 
+#============================================================================
+function Take-Count() {
+    param ( [int]$count = $(throw "Need a count") )
+    begin { 
+        $total = 0;
+    }
+    process { 
+        if ( $total -lt $count ) {
+            $_
+        }
+        $total += 1
+    }
+}
+
+#============================================================================
+# Take elements from the pipeline while the predicate is true
+#============================================================================
+function Take-While() {
+    param ( [scriptblock]$pred = $(throw "Need a predicate") )
+    begin {
+        $take = $true
+    }
+    process {
+        if ( $take ) {
+            $take = & $pred $_
+            if ( $take ) {
+                $_
+            }
+        }
+    }
+}
+
+$lines = Get-Content .\ReleaseNotes.md | Where-Object { $_.Length -gt 0 }
+$top = $lines | Select-Object -First 1
+$version = $top.Replace("#", "").Trim()
+$releaseNotes = ""
+
+$notes = $lines | Select-Object -Skip 1 | Take-While { -not $_.Trim().StartsWith("#") }
+
+foreach ($note in $notes) {
+    $releaseNotes += $note + "`n"
+}
+
 dotnet restore
 dotnet test ScrapySharp.Tests\ScrapySharp.Tests.csproj
 dotnet build --configuration release
-dotnet pack --configuration release
-Remove-Item -Recurse -Force release
+dotnet pack --configuration release /p:PackageVersion=$version /p:PackageReleaseNotes=$releaseNotes
+if (Test-Path .\release)
+{
+    Remove-Item -Recurse -Force -Path release
+}
 mkdir release
 xcopy .\ScrapySharp\bin\Release\*.nupkg release
 Remove-Item .\ScrapySharp\bin\**\*.nupkg
