661ee6fab6f52dea99c798ba882b87a1aa2daf66
rflechner <rflechner@romcyber.com>
2018-04-05 21:11:24 +0200

    builds

--- build.cmd
+++ build.cmd
@@ -1,4 +1,9 @@
-@echo off
-cls
-".nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
-"packages\FAKE\tools\Fake.exe" build.fsx %*
+@rem @echo off
+@rem cls
+@rem ".nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
+@rem "packages\FAKE\tools\Fake.exe" build.fsx %*
+
+dotnet restore
+dotnet test ScrapySharp.Tests\ScrapySharp.Tests.csproj
+dotnet build --configuration release
+dotnet pack --configuration release
