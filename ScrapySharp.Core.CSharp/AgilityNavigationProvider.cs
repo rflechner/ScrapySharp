e1027ca014e127bf0699195a0fdac1a797c11648
HumbertoML <humberto_molinalopez@hotmail.com>
2021-09-22 22:21:05 -0500

    Mostrando mas explicito el valor de la clase NavigationProvider

--- ScrapySharp.Core.CSharp/AgilityNavigationProvider.cs
+++ ScrapySharp.Core.CSharp/AgilityNavigationProvider.cs
@@ -2,7 +2,6 @@
 using Microsoft.FSharp.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-using static ScrapySharp.Core.CSharp.NavigationProvider;
 
 namespace ScrapySharp.Core.CSharp
 {
@@ -13,19 +12,19 @@ namespace ScrapySharp.Core.CSharp
         }
         List<HtmlNode> INavigationProvider<HtmlNode>.ChildNodes(List<HtmlNode> nodes)
         {
-            return new List<HtmlNode>(SeqModule.Collect<HtmlNodeCollection, HtmlNodeCollection, HtmlNode>(ChildNodes23._instance, SeqModule.Map<HtmlNode, HtmlNodeCollection>(ChildNodes231._instance, nodes)));
+            return new List<HtmlNode>(SeqModule.Collect<HtmlNodeCollection, HtmlNodeCollection, HtmlNode>(NavigationProvider.ChildNodes23._instance, SeqModule.Map<HtmlNode, HtmlNodeCollection>(NavigationProvider.ChildNodes231._instance, nodes)));
         }
         List<HtmlNode> INavigationProvider<HtmlNode>.Descendants(List<HtmlNode> nodes)
         {
-            return new List<HtmlNode>(SeqModule.Collect<IEnumerable<HtmlNode>, IEnumerable<HtmlNode>, HtmlNode>(Descendants26._instance, SeqModule.Map<HtmlNode, IEnumerable<HtmlNode>>(Descendants261._instance, nodes)));
+            return new List<HtmlNode>(SeqModule.Collect<IEnumerable<HtmlNode>, IEnumerable<HtmlNode>, HtmlNode>(NavigationProvider.Descendants26._instance, SeqModule.Map<HtmlNode, IEnumerable<HtmlNode>>(NavigationProvider.Descendants261._instance, nodes)));
         }
         List<HtmlNode> INavigationProvider<HtmlNode>.ParentNodes(List<HtmlNode> nodes)
         {
-            return new List<HtmlNode>(SeqModule.Map<HtmlNode, HtmlNode>(ParentNodes29._instance, nodes));
+            return new List<HtmlNode>(SeqModule.Map<HtmlNode, HtmlNode>(NavigationProvider.ParentNodes29._instance, nodes));
         }
         List<HtmlNode> INavigationProvider<HtmlNode>.AncestorsAndSelf(List<HtmlNode> nodes)
         {
-            return new List<HtmlNode>(SeqModule.Collect<IEnumerable<HtmlNode>, IEnumerable<HtmlNode>, HtmlNode>(AncestorsAndSelf32._instance, SeqModule.Map<HtmlNode, IEnumerable<HtmlNode>>(AncestorsAndSelf321._instance, nodes)));
+            return new List<HtmlNode>(SeqModule.Collect<IEnumerable<HtmlNode>, IEnumerable<HtmlNode>, HtmlNode>(NavigationProvider.AncestorsAndSelf32._instance, SeqModule.Map<HtmlNode, IEnumerable<HtmlNode>>(NavigationProvider.AncestorsAndSelf321._instance, nodes)));
         }
         string INavigationProvider<HtmlNode>.GetName(HtmlNode node)
         {
